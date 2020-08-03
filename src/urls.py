ddc3e0d85485188c4a0a703480d10891cb1a4488
ptran068 <ptran068@gmail.com>
2020-08-03 14:29:06 +0700

    init

--- src/urls.py
+++ src/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
+    path('file/', include('files.urls')),
 
 ]
-
